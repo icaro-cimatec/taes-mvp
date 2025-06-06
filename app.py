2261dfd95a68e404038bea58b73675bfd08f6644
Ícaro Yan Rosário da Silva <icaro.rosario@fieb.org.br>
2025-06-06 17:51:13 -0300

    feat: cadastrar produto

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+products = []
+
+@app.route('/api/products', methods=['POST'])
+def add_product():
+    data = request.json
+    products.append({
+        'name': data['name'],
+        'description': data['description'],
+        'price': data['price'],
+        'artisan': data['artisan']
+    })
+    return jsonify({ 'message': 'Produto cadastrado com sucesso' }), 201
+
+
+@app.route('/api/products', methods=['GET'])
+def list_products():
+    return jsonify(products)
 
 if __name__ == '__main__':
     app.run(debug=True)
