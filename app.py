016ede75746b75c9bd123cf4f3d67001a6a4d3ee
Ícaro Yan Rosário da Silva <icaro.rosario@fieb.org.br>
2025-06-06 18:01:28 -0300

    feat: login como artesão

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
 
 app = Flask(__name__)
 CORS(app)
+
+app.config['SECRET_KEY'] = 'segredo_devsecops'
 
 users = []
 products = []
@@ -19,6 +23,19 @@ def register():
         'password': hashed_password
     })
     return jsonify({ 'message': 'Artesão cadastrado com sucesso' }), 201
+
+# Rota de login
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.json
+    user = next((u for u in users if u['email'] == data['email']), None)
+    if not user or not check_password_hash(user['password'], data['password']):
+        return jsonify({ 'error': 'Credenciais inválidas' }), 401
+    token = jwt.encode({
+        'email': user['email'],
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+    }, app.config['SECRET_KEY'], algorithm='HS256')
+    return jsonify({ 'token': token })
 
 @app.route('/api/products', methods=['POST'])
 def add_product():
