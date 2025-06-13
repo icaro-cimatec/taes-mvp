7b656eb748927d684ceed6c60514c3ba3b158b63
icaro-cimatec <icaro.y.silva@ba.estudante.senai.br>
2025-06-13 15:43:03 -0300

    feat: adicionar SQLAlchemy e configuração com dotenv

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from app.config import Config
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
 CORS(app)
 
 if __name__ == '__main__':
+    app.config.from_object(Config)
+
+    db.init_app(app)
+
     app.run(debug=True)
