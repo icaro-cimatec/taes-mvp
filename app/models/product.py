d31d3b1c557d6d78bffc27b5126d02bec3237503
icaro-cimatec <icaro.y.silva@ba.estudante.senai.br>
2025-06-13 15:47:50 -0300

    chore: criar modelo base de User e Product

---
+++ app/models/product.py
@@ -0,0 +1,8 @@
+from app import db
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    price = db.Column(db.Float)
+    artisan_email = db.Column(db.String(100))
--- run.py
+++ run.py
@@ -1,16 +1,6 @@
-from flask import Flask
-from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-from app.config import Config
+from app import create_app
 
-db = SQLAlchemy()
-
-app = Flask(__name__)
-CORS(app)
+app = create_app()
 
 if __name__ == '__main__':
-    app.config.from_object(Config)
-
-    db.init_app(app)
-
     app.run(debug=True)
