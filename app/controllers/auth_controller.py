6eb45dc97a365c5a6766a21470e89e8ca8e906ea
icaro-cimatec <icaro.y.silva@ba.estudante.senai.br>
2025-06-13 16:08:35 -0300

    feat: criar rota de login com geração de JWT

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -1,6 +1,9 @@
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
 from app import db
 from app.models.user import User
+from app.config import Config
 
 def create_user(data):
     user = User(
@@ -11,3 +14,16 @@ def create_user(data):
     db.session.add(user)
     db.session.commit()
     return user
+
+def get_user(data):
+    user = User.query.filter_by(email=data['email']).first()
+
+    if not user or not check_password_hash(user.password, data['password']):
+        return None
+
+    token = jwt.encode({
+        'email': user.email,
+        'exp': datetime.datetime.now(datetime.UTC) + datetime.timedelta(hours=1)
+    }, Config.SECRET_KEY, algorithm=['HS256'])
+
+    return token
--- app/views/auth_view.py
+++ app/views/auth_view.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from app.controllers.auth_controller import create_user
+from app.controllers.auth_controller import create_user, get_user
 
 auth_bp = Blueprint('auth', __name__)
 
@@ -8,3 +8,13 @@ def register():
     data = request.json
     user = create_user(data)
     return jsonify({'message': 'Usuário cadastrado com sucesso', 'email': user.email})
+
+@auth_bp.route('/api/login', methods=['GET'])
+def login():
+    data = request.json
+    token = get_user(data)
+
+    if not token:
+        jsonify({'error':'Credenciais inválidas'}), 401
+    else:
+        return jsonify({'token': token})
