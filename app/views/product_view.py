399b3b2ec0f57a70e41d6be89a218301ad82a5f6
icaro-cimatec <icaro.y.silva@ba.estudante.senai.br>
2025-06-13 18:08:24 -0300

    fix: ajustes em warnings

--- app/views/product_view.py
+++ app/views/product_view.py
@@ -1,13 +1,15 @@
 from flask import Blueprint, request, jsonify
-from app.controllers.product_controller import create_product, get_products as get_products_controller
-
+from app.controllers.product_controller import create_product
+from app.controllers.product_controller import get_products as get_products_controller
 product_bp = Blueprint('product', __name__)
+
 
 @product_bp.route('/api/products', methods=['POST'])
 def add_product():
     data = request.json
     product = create_product(data)
     return jsonify({'message': 'Produto cadastrado', 'id': product.id}), 201
+
 
 @product_bp.route('/api/products', methods=['GET'])
 def get_products():
