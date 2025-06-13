71b73462a084bf1d37fb127f321c25bc61932822
icaro-cimatec <icaro.y.silva@ba.estudante.senai.br>
2025-06-13 20:07:03 -0300

    feat: adicionar controller para salvar e listar comentários

---
+++ app/controllers/comment_controller.py
@@ -0,0 +1,16 @@
+from app import db
+from app.models.comment import Comment
+
+def add_comment(data):
+    comment = Comment(
+        product_id=data['product_id'],
+        author=data['author'],
+        content=data['content'],
+        rating=data['rating']
+    )
+    db.session.add(comment)
+    db.session.commit()
+    return comment
+
+def get_comments_by_product(product_id):
+    return Comment.query.filter_by(product_id=product_id).all()
